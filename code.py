f28777a344a9b1bb0c30fdba45a1c9e5df55e2a3
Masa Jalamneh <159816449+MasaJalamneh@users.noreply.github.com>
2025-06-05 15:45:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
-Hello 
+import pandas as pd
+
+# Load the CSV file
+df = pd.read_csv("C:\\Users\\hp\\Desktop\\NLP_Proj\\Amazon_Reviews.csv")
+
+# Display the shape and the first few rows
+print("Shape of the dataset:", df.shape)
+print(df.head())
